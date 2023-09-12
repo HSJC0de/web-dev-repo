e67ab0cc7bb08b9f2640ba785fce08d6177c662c
Joshua D <email@email.com>
2023-09-12 14:00:44 -0700

    wrote js to create the posts but it doesn not work

--- posts-project/script.js
+++ posts-project/script.js
@@ -1,4 +1,45 @@
 const titleInputElement = document.querySelector('#titleArea');
-const textInputElement = document.quearySelector('#inputArea');
+const textInputElement = document.querySelector('#inputArea');
 const insertButtonElement = document.querySelector('#id-button-insert');
-const postButtonElement = document.querySelector('#id-button-post');
+let postButtonElement = document.querySelector('#id-button-post');
+
+//add 'posts' to this element
+const containerPostsElement = document.querySelector('.container-posts');
+
+/*
+postButtonElement.addEventListener('click', function (){
+  console.log(titleInputElement.value);
+  console.log(textInputElement.value);
+  titleInputElement.value = '';
+  textInputElement.value = '';
+}) */
+
+
+function createPost(){
+  const title = titleInputElement.value;
+  const text = textInputElement.value;
+  console.log(titleInputElement.value);
+  console.log(textInputElement.value);
+  /*
+  containerPostsElement.innerHTML += 
+  `
+  <div class="post">
+    <div>
+      <p class="container-post-title">${title}</p>
+    </div>
+    <div class="container-post-text">
+      <p>${text}</p>
+    </div>
+    <div class="container-post-image">
+      <img class="post-image" src="images/post-image-eiffel-tower.jpg">
+    </div>
+  </div>
+  `;
+  */
+  console.log('test');
+  //postButtonElement = document.querySelector('#id-button-post');
+}
+
+postButtonElement.addEventListener('click', createPost);
+
+//problem - only posting once, cant post again
