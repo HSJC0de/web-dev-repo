fb1fbd8eeb63eeb3d73dcba961bad0ecede170b5
Joshua D <email@email.com>
2023-09-12 11:39:11 -0700

    created id for insert and post button, created const var for titleInpit, texINput, insert and post button

---
+++ posts-project/script.js
@@ -0,0 +1,4 @@
+const titleInputElement = document.querySelector('#titleArea');
+const textInputElement = document.quearySelector('#inputArea');
+const insertButtonElement = document.querySelector('#id-button-insert');
+const postButtonElement = document.querySelector('#id-button-post');
