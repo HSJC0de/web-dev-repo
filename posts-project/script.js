6dbb3c0f77cd8adcd0819403d78045e5f9a29a1d
HSJC0de <delacruz.joshuaprsnl@gmail.com>
2023-09-12 18:18:30 -0700

    removed post class from html, put what post class consists of in comments in script.js

--- posts-project/script.js
+++ posts-project/script.js
@@ -6,6 +6,20 @@ let postButtonElement = document.querySelector('#id-button-post');
 //add 'posts' to this element
 const containerPostsElement = document.querySelector('.container-posts');
 
+/* this is what a post section consists of
+<div class="post">
+          <div>
+            <p class="container-post-title">Title</p>
+          </div>
+          <div class="container-post-text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div class="container-post-image">
+            <img class="post-image" src="images/post-image-eiffel-tower.jpg">
+          </div>
+        </div>
+*/
+
 function createPost(){
   const title = titleInputElement.value;
   const text = textInputElement.value;
