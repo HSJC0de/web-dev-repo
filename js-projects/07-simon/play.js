bd3570864813fe745c392d4bef98b3677e36c063
HSJC0de <delacruz.joshuaprsnl@gmail.com>
2023-10-15 00:48:39 -0700

    set up html and started working on js, stoppped working on the count

---
+++ js-projects/07-simon/play.js
@@ -0,0 +1,12 @@
+const greenSquare = document.getElementById('green');
+const redSquare = document.getElementById('red');
+const yellowSquare = document.getElementById('yellow');
+const blueSquare = document.getElementById('blue');
+const startButton = document.getElementById('startButton');
+const count = 
+
+startButton.addEventListener('click', runGame);
+
+function runGame(){
+
+}
